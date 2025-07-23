6efc925f3321a47cf9d4a5954ba1ecb512d4090c
fianza386 <fianza386@gmail.com>
2025-07-23 21:57:22 +0530

    first commit

--- app/(user)/register/[account]/page.jsx
+++ app/(user)/register/[account]/page.jsx
@@ -1,15 +1,63 @@
 "use client"
+
 import Web3Context from "@/components/web3context";
 import { ethers } from "ethers";
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Swal from "sweetalert2";
+
 
 
 
 const Register = ({params}) => {
  const { account } = React.use(params);
  const {selectedAccount,logicContract,storageContract,teamBussinessContract,stakeTokenContract,stakeUSDTContract}=useContext(Web3Context)
- 
+ const [currentPrice,setCurrentPrice]=useState(0);
+ const [netBVT,setNetBVT]=useState(0);
+
+
+ useEffect(() => {
+    const fetchPrice = async () => {
+    //   const resp = await fetch('/api/bvt-price'); // No CORS issue
+    //   const { price } = await resp.json();
+    //   console.log(price)
+      setCurrentPrice(0.52);
+    };
+    fetchPrice();
+  }, []);
+
+const fn_CalculateBVT=async(e)=>{
+    const pkgid=e.target.value;
+    if(pkgid==""){
+        alert("select package")
+        return;
+    }
+    let amount =0;
+    if(pkgid==0){
+        amount=54
+    }
+    else if(pkgid==1){
+         amount=215
+    }
+    else if(pkgid==2){
+         amount=5000
+    }
+    else if(pkgid==3){
+         amount=25000
+    }
+    else{
+          alert("select package")
+        return;
+    }
+    if(currentPrice>0){
+        if(pkgid==0 || pkgid==1){
+            const totalbvt=Number(amount)/Number(currentPrice)
+            setNetBVT(totalbvt)
+        }
+        else{
+            setNetBVT(amount)
+        }
+    }
+}
  const fn_submit=async(e)=>{
 e.preventDefault();
 const pkgDropdown=document.querySelector("#ddPackage")
@@ -20,7 +68,7 @@ if(pkgDropdown.value==""){
 }
 
 try{
-    debugger
+    
     const {_status}=await storageContract.GetUser(account)    
     if(!_status){
         alert("Invalid sponsor")
@@ -33,10 +81,20 @@ try{
     else if(pkgDropdown.value==1){
          amount="215"
     }
+    else if(pkgDropdown.value==2){
+         amount="5000"
+    }
+    else if(pkgDropdown.value==3){
+         amount="25000"
+    }
     else{
           alert("select package")
         return;
     }
+
+    
+
+
     const approve_tx=await stakeTokenContract.approve(process.env.NEXT_PUBLIC_STORAGE_CONTRACT,ethers.parseUnits(amount, 18));
     const approve_reciept=await approve_tx.wait();
     if(approve_reciept){
@@ -71,6 +129,8 @@ catch(err){
 }
 
 
+
+
  }
 
 
@@ -84,11 +144,21 @@ catch(err){
             <div className='row'>
                 <div className='form-group'>
                     <label>Package</label>
-                    <select className='form-control' id='ddPackage' name='package'>
+                    <select className='form-control' id='ddPackage' onChange={fn_CalculateBVT} name='package'>
                         <option value="">Select Package</option>
-                        <option value="0">54 USDT</option>
-                        <option value="1">215 USDT</option>
+                        <option value="0">54 USDT (BVT) NORMAL</option>
+                        <option value="1">215 USDT (BVT) MANAGER</option>
+                        <option value="2">5000 BVT SUPER MANAGER</option>
+                        <option value="3">25000 BVT DIAMOND</option>
                     </select>
+                </div>
+                 <div className='form-group'>
+                    <label>Current BVT Price:</label>
+                    <input type='text' className="form-control" readOnly value={currentPrice} />
+                </div>
+                  <div className='form-group'>
+                    <label>Net BVT:</label>
+                    <input type='text' className="form-control" readOnly value={netBVT} />
                 </div>
             </div>
         </div>
---
+++ pages/api/bvt-price.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  try {
+    const { data } = await axios.get(
+      'https://api.mexc.com/api/v3/ticker/price?symbol=BVTUSDT' // test with BTC first
+    );
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('MEXC Fetch Error:', error.message);
+    res.status(500).json({ error: 'Failed to fetch price' });
+  }
+}
